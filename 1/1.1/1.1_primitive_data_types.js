0862a05516eb64c99949af91815d503f37d4a1f5
Hanan <anow147@gmail.com>
2023-01-08 13:20:49 +0200

    initialize

---
+++ 1/1.1/1.1_primitive_data_types.js
@@ -0,0 +1,14 @@
+"use strict";
+let age = 22;
+let yes = true;
+let myName = "Hanan";
+let empty = null;
+let undefined;
+let symbolValue = Symbol("only");
+
+console.log(typeof myName);
+console.log(typeof age);
+console.log(typeof yes);
+console.log(typeof empty);
+console.log(typeof undefined);
+console.log(typeof symbolValue);
