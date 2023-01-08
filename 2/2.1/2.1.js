d769d81bf600ed64611d11dc43bf69a35d8d6e6d
Hanan <anow147@gmail.com>
2023-01-08 14:40:55 +0200

    2.1

---
+++ 2/2.1/2.1.js
@@ -0,0 +1,6 @@
+function countryInfo(country, population, capitalCity) {
+  return `${country} has ${population} millions people and its capital city is ${capitalCity}`;
+}
+console.log(countryInfo("Israel", 9, "Jerusalem"));
+console.log(countryInfo("France", 9, "Paris"));
+console.log(countryInfo("Jordan", 7, "Amman"));
