9f0383d2b2cf0ae8e22bad0b3ccfc8384191aaba
Hanan <anow147@gmail.com>
2023-01-08 14:50:23 +0200

    2.1_update

--- 2/2.1/2.1.js
+++ 2/2.1/2.1.js
@@ -1,6 +1,10 @@
 function countryInfo(country, population, capitalCity) {
   return `${country} has ${population} millions people and its capital city is ${capitalCity}`;
 }
-console.log(countryInfo("Israel", 9, "Jerusalem"));
-console.log(countryInfo("France", 9, "Paris"));
-console.log(countryInfo("Jordan", 7, "Amman"));
+let israel = countryInfo("Israel", 9, "Jerusalem");
+let france = countryInfo("France", 60, "Paris");
+let jordan = countryInfo("Jordan", 7, "Amman");
+
+console.log(israel);
+console.log(france);
+console.log(jordan);
