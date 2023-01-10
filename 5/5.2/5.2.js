b07656a8db3f9d2b74a69a7bb69cfab0b1fe0b96
Hanan <anow147@gmail.com>
2023-01-10 14:15:28 +0200

    5.2

---
+++ 5/5.2/5.2.js
@@ -0,0 +1,28 @@
+function languageInfo(language) {
+  let info;
+
+  switch (language) {
+    case "Mandarin":
+      info = "MOST number of native speakers!";
+      break;
+    case "Spanish":
+      info = "2nd place in a number of native speakers";
+      break;
+    case "English":
+      info = "3rd place";
+      break;
+    case "Hindi":
+      info = "Number 4";
+      break;
+    case "Arabic":
+      info = "5th most spoken language";
+      break;
+    default:
+      info = "Not in the top 5";
+      break;
+  }
+
+  return info;
+}
+
+console.log(languageInfo("arabic"));
